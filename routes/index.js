016418b4aa90d476dd19ec6f66b2bf3d0276ed57
nguyendo140201 <npham140201@dophamnguyen.xyz>
2023-07-12 16:12:33 +0700

    first commit

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+const dayjs = require("dayjs");
+var express = require("express");
+require("dotenv").config();
+var router = express.Router();
+var app = express();
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  const date = dayjs().format(" YYYY-MM-DDT HH");
+  if (process.env.TOKEN === req.headers.TOKEN) {
+    if (date !== app.get("time")) {
+      require("crypto").randomBytes(48, function (err, buffer) {
+        var token = buffer.toString("hex");
+        app.set("token", token);
+        app.set("time", date);
+        res.status(200).json({ token: token });
+      });
+    } else {
+      res.status(200).json({ token: app.get("token") });
+    }
+  } else {
+    res.status(401).json({ err: "lost token" });
+  }
+});
+
+module.exports = router;
